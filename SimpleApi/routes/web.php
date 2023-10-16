9df9a00a8009f16cdd11b9e7d7d8be85cd8ca76f
Maria Clara Barroso <maria.barroso@midr.gov.br>
2023-10-16 18:05:49 -0300

    Add new Api

---
+++ SimpleApi/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Client\Request;
+use App\Http\Controllers\BookController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request){
+    return $request->user();
+});
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books{id}', [BookController::class, 'destroy']);
